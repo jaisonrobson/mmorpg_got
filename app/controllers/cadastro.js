fe56cc6c010cf7902187375a60d01c47445e81b4
jaisonrobson <xd_jrg@hotmail.com>
2024-06-21 17:27:46 -0300

    feat: preparado formulario da view de cadastro & respectivo controller para tratar e persistir dados

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,3 +1,22 @@
 module.exports.cadastro = function(application, req, res){
-    res.render('cadastro');
+    res.render('cadastro', { validacao: {}, dadosForm: {} });
+}
+
+module.exports.cadastrar = function(application, req, res){
+    var dadosForm = req.body;
+
+    req.assert('nome', 'Nome nao pode ser vazio').notEmpty();
+    req.assert('usuario', 'Usuario nao pode ser vazio').notEmpty();
+    req.assert('senha', 'Senha nao pode ser vazio').notEmpty();
+    req.assert('casa', 'Casa nao pode ser vazio').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if (erros) {
+        res.render('cadastro', { validacao: erros, dadosForm: dadosForm });
+
+        return;
+    }
+
+    res.send('podemos cadastrar');
 }

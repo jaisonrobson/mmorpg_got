dd28e556e14308d8241160b519ab3a4809107e15
jaisonrobson <xd_jrg@hotmail.com>
2024-06-27 18:41:44 -0300

    feat: ajustado pagina 'jogo' para validar se usuario esta autenticado

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,3 +1,10 @@
 module.exports.jogo = function(application, req, res){
-    res.render('jogo');
+
+    if (req.session.autorizado) {
+        res.render('jogo');
+    }
+    else
+    {
+        res.send('Usuario nao esta logado.');
+    }
 }
